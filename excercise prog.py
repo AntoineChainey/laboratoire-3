d56629b230679ba950f6d3b787e3b79113f224a8
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-02-05 13:25:39 -0500

    Update excercise prog.py

--- excercise prog.py
+++ excercise prog.py
@@ -1,6 +1,6 @@
 age = input("entrer votre age: ")  
 age = float(age)                    
-if age == 18 or age >= 18 :         
+if age < 18 :         
     print("vous avez",age,"ans")    
     consommation = input("buver vous de l'alcool: ")        
     if consommation == "oui" :                             
@@ -13,8 +13,60 @@ if age == 18 or age >= 18 :
             print("bravo merci pour votre réponse")
         else:
             print("réponse incorect")           
+    elif consommation == "non":
+         print("bravo") 
     else:
-        print("bravo")                  
+        print("réponse incorrect")                      
+elif age >= 18 and age < 30 :
+    print("vous avez",age,"ans")    
+    consommation = input("buver vous de l'alcool: ")        
+    if consommation == "oui" :                             
+        rythmeconsommation = input("a quel fréquence consommer vous: ")     
+        if rythmeconsommation == "chaque jour":                             
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque semaine":
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque mois" or rythmeconsommation == "autre":
+            print("bravo merci pour votre réponse")
+        else:
+            print("réponse incorect")           
+    elif consommation == "non":
+         print("bravo") 
+    else:
+        print("réponse incorrect")
+elif age >= 30 and age < 50 :
+    print("vous avez",age,"ans")    
+    consommation = input("buver vous de l'alcool: ")        
+    if consommation == "oui" :                             
+        rythmeconsommation = input("a quel fréquence consommer vous: ")     
+        if rythmeconsommation == "chaque jour":                             
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque semaine":
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque mois" or rythmeconsommation == "autre":
+            print("bravo merci pour votre réponse")
+        else:
+            print("réponse incorect")           
+    elif consommation == "non":
+         print("bravo") 
+    else:
+        print("réponse incorrect")
+elif age >= 50 :
+    print("vous avez",age,"ans")    
+    consommation = input("buver vous de l'alcool: ")        
+    if consommation == "oui" :                             
+        rythmeconsommation = input("a quel fréquence consommer vous: ")     
+        if rythmeconsommation == "chaque jour":                             
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque semaine":
+            print("merci pour votre réponse")
+        elif rythmeconsommation == "chaque mois" or rythmeconsommation == "autre":
+            print("bravo merci pour votre réponse")
+        else:
+            print("réponse incorect")           
+    elif consommation == "non":
+         print("bravo") 
+    else:
+        print("réponse incorrect")
 else :
-    print("vous ête mineur")        
-
+    print("réponse incorect")
