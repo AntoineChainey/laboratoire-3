a1dff90d41c0fcd4f6530da7692e0b5ebc887f37
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-02-05 14:59:12 -0500

    Create modification.py

---
+++ modification.py
@@ -0,0 +1,5 @@
+"""le questionnaire est maintenant dans une boucle ce qui fais que lorqu'une entré est incorrect l'utilisateur doit entré une autre réponse.
+cette modification est fait a l'aide de while.
+les étapes sont tous commentées.
+j'ai ajouté d'autre catégorie pour mieux repérer les tendance de chaque groupe.
+avec les if et des comparaison."""
