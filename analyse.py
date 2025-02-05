3a0048598120c320d3a7e0533ef332a6525d0e97
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-02-05 12:59:18 -0500

    Create analyse.py

---
+++ analyse.py
@@ -0,0 +1,6 @@
+""" 1:séparer dans différente catégorie d’age :
+Mineur, moins de 30 ans entre 30 et 50 plus que 50.
+2 :Lorsque réponse incorrect renvoie a la question.
+3 :Lorsque le code finis renvoie au début
+4 :Commenter le code
+5 :clarifier les entrer possible """
